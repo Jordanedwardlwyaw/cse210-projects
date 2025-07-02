eb28c4aed3e0d7142b5f3601e1c36b5bd62e5396
Jordanedwardlwyaw <jlwaya@byupathwy.edu>
2025-07-02 12:24:46 +0300

    add

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+
+        string letter = "";
+        string sign = "";
+
+        
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! Try again and you'll improve next time.");
+        }
+
+        int lastDigit = gradePercentage % 10;
+
+        if (letter != "A" && letter != "F") 
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (letter == "A" && gradePercentage < 93)
+        {
+        
+            sign = "-";
+        }
+
+        Console.WriteLine($"Your final grade is: {letter}{sign}");
     }
 }
