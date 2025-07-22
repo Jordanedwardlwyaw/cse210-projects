e86fe3c3f50e1fe7a2364485b48e965d1a6c5528
Jordanedwardlwyaw <jlwaya@byupathwy.edu>
2025-07-22 13:32:49 +0300

    add

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        // Create a list to hold videos
+        List<Video> videos = new List<Video>();
+
+        // Create sample videos
+        Video video1 = new Video("Exploring Uganda", "Jordan Edward", 420);
+        video1.AddComment(new Comment("Alice", "Great video! Love the culture."));
+        video1.AddComment(new Comment("Brian", "Nice editing!"));
+        video1.AddComment(new Comment("Clara", "I want to visit Uganda now."));
+
+        Video video2 = new Video("Tech Tips 2025", "TechDaily", 600);
+        video2.AddComment(new Comment("Daniel", "Very helpful tips, thanks!"));
+        video2.AddComment(new Comment("Emma", "Waiting for the next one."));
+        video2.AddComment(new Comment("Frank", "Subscribed!"));
+
+        Video video3 = new Video("Cooking Matoke", "Mama Africa", 300);
+        video3.AddComment(new Comment("Grace", "Looks delicious!"));
+        video3.AddComment(new Comment("Henry", "My grandma used to cook this."));
+        video3.AddComment(new Comment("Ivy", "Trying this tonight!"));
+
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        // Display video information
+        foreach (Video video in videos)
+        {
+            video.Display();
+            Console.WriteLine();
+        }
     }
 }
---
+++ week04/YouTubeVideos/video.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+class Video
+{
+    private string _title;
+    private string _author;
+    private int _lengthInSeconds;
+    private List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int lengthInSeconds)
+    {
+        _title = title;
+        _author = author;
+        _lengthInSeconds = lengthInSeconds;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return _comments.Count;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Length: {_lengthInSeconds} seconds");
+        Console.WriteLine($"Comments ({GetNumberOfComments()}):");
+        foreach (Comment comment in _comments)
+        {
+            comment.Display();
+        }
+    }
+}
